741abe8adc35a0945efc1050a84ad7f2630d8796
Dax Pandhi <dax@quadspinner.com>
2022-11-17 22:41:56 +0530

    Important bugfix

--- src/Commands/CreateHighlight.cs
+++ src/Commands/CreateHighlight.cs
@@ -5,113 +5,119 @@ using System.Windows;
 
 namespace Highlighter.Commands
 {
-    [Command(PackageIds.CreateHighlight)]
-    internal sealed class CreateHighlight : BaseCommand<CreateHighlight>
-    {
-        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
-        {
-            var dte = (DTE)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(DTE));
-            string selection = (dte.ActiveDocument.Selection as TextSelection).Text;
+	[Command(PackageIds.CreateHighlight)]
+	internal sealed class CreateHighlight : BaseCommand<CreateHighlight>
+	{
+		protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+		{
+			var dte = (DTE)Microsoft.VisualStudio.Shell.Package.GetGlobalService(typeof(DTE));
+			string selection = (dte.ActiveDocument.Selection as TextSelection).Text;
 
-            if (string.IsNullOrEmpty(selection) || string.IsNullOrWhiteSpace(selection))
-                return;
+			if (string.IsNullOrEmpty(selection) || string.IsNullOrWhiteSpace(selection))
+				return;
 
-            var options = Options.GetLiveInstanceAsync().Result;
-            options.ColorTags ??= Helper.GetFillerTags().ToArray();
+			var options = Options.GetLiveInstanceAsync().Result;
+			options.ColorTags ??= Helper.GetFillerTags().ToArray();
 
-            var tags = options.ColorTags.ToList();
-            var slntags = options.SolutionTags?.ToList();
-            var found = tags.FirstOrDefault(x => x.Criteria == selection);
-            bool isNew = false;
-            bool notGlobal = false;
+			var tags = options.ColorTags.ToList();
+			var slntags = options.SolutionTags?.ToList();
+			var found = tags.FirstOrDefault(x => x.Criteria == selection);
+			bool isNew = false;
+			bool notGlobal = false;
 
-            // If tag is null, do a second search in Solution-level tags
-            if (found == null)
-            {
-                found = slntags.FirstOrDefault(x => x.Criteria == selection);
+			// If tag is null, do a second search in Solution-level tags
+			if (found == null)
+			{
+				try
+				{
+					found = slntags?.FirstOrDefault(x => x.Criteria == selection);
+					if (found != null)
+						notGlobal = true;
+				}
+				catch (Exception)
+				{
+					notGlobal = true;
+				}
+			}
 
-                if (found != null)
-                    notGlobal = true;
-            }
+			// No tags were found, so make a new tag
+			isNew = found == null;
 
-            // No tags were found, so make a new tag
-            isNew = found == null;
+			found ??= new HighlightTag(selection)
+			{
+				Blur = Remember.LastBlur,
+				Shape = Remember.LastShape
+			};
 
-            found ??= new HighlightTag(selection)
-            {
-                Blur = Remember.LastBlur,
-                Shape = Remember.LastShape
-            };
+			// Prepare the editor
+			var editor = new EditColor
+			{
+				// The tag to edit
+				TagToEdit = found,
+				// Is it an existing rule?
+				btnModify = { Content = isNew ? "Create" : "Modify" },
+				// Allow deleting of existing rule
+				btnDelete = { Visibility = isNew ? Visibility.Collapsed : Visibility.Visible },
+				// Appropriate title
+				Title = isNew ? "Create Highlight Rule" : "Modify Highlight Rule",
+				// Whether rule is global or not, disallow editing of existing rule
+				chkGlobal = { IsChecked = isNew ? Remember.LastGlobal : !notGlobal, IsEnabled = isNew },
+				// Whether partial matches are allowed
+				chkPartial = { IsChecked = found.AllowPartialMatch },
+				chkCaseSensitive = { IsChecked = found.IsCaseSensitive }
+			};
 
-            // Prepare the editor
-            var editor = new EditColor
-            {
-                // The tag to edit
-                TagToEdit = found,
-                // Is it an existing rule?
-                btnModify = { Content = isNew ? "Create" : "Modify" },
-                // Allow deleting of existing rule
-                btnDelete = { Visibility = isNew ? Visibility.Collapsed : Visibility.Visible },
-                // Appropriate title
-                Title = isNew ? "Create Highlight Rule" : "Modify Highlight Rule",
-                // Whether rule is global or not, disallow editing of existing rule
-                chkGlobal = { IsChecked = isNew ? Remember.LastGlobal : !notGlobal, IsEnabled = isNew },
-                // Whether partial matches are allowed
-                chkPartial = { IsChecked = found.AllowPartialMatch },
-                chkCaseSensitive = { IsChecked = found.IsCaseSensitive }
-            };
+			bool result = editor.ShowDialog().Value;
 
-            bool result = editor.ShowDialog().Value;
+			if (result)
+			{
+				if (isNew)
+				{
+					if (editor.chkGlobal.IsChecked == true)
+					{
+						// CREATE GLOBAL TAG
+						tags.Add(editor.TagToEdit);
+						options.ColorTags = tags.ToArray();
+					}
+					else
+					{
+						// CREATE SOLUTION TAG
+						slntags.Add(editor.TagToEdit);
+						options.SolutionTags = slntags.ToArray();
+					}
 
-            if (result)
-            {
-                if (isNew)
-                {
-                    if (editor.chkGlobal.IsChecked == true)
-                    {
-                        // CREATE GLOBAL TAG
-                        tags.Add(editor.TagToEdit);
-                        options.ColorTags = tags.ToArray();
-                    }
-                    else
-                    {
-                        // CREATE SOLUTION TAG
-                        slntags.Add(editor.TagToEdit);
-                        options.SolutionTags = slntags.ToArray();
-                    }
+					// Remember user preference
+					Remember.LastGlobal = editor.chkGlobal.IsChecked == true;
+					Remember.LastBlur = found.Blur;
+					Remember.LastShape = found.Shape;
+				}
 
-                    // Remember user preference
-                    Remember.LastGlobal = editor.chkGlobal.IsChecked == true;
-                    Remember.LastBlur = found.Blur;
-                    Remember.LastShape = found.Shape;
-                }
+				// Save to options
+				await options.SaveAsync();
+			}
+			else
+			{
+				// User wants to delete the rule
+				if (editor.delete)
+				{
+					// Check if it is a global rule
+					if (notGlobal)
+					{
+						// It is a solution-level rule
+						slntags.Remove(found);
+						options.SolutionTags = slntags.ToArray();
+					}
+					else
+					{
+						// It is a global rule
+						tags.Remove(found);
+						options.ColorTags = tags.ToArray();
+					}
 
-                // Save to options
-                await options.SaveAsync();
-            }
-            else
-            {
-                // User wants to delete the rule
-                if (editor.delete)
-                {
-                    // Check if it is a global rule
-                    if (notGlobal)
-                    {
-                        // It is a solution-level rule
-                        slntags.Remove(found);
-                        options.SolutionTags = slntags.ToArray();
-                    }
-                    else
-                    {
-                        // It is a global rule
-                        tags.Remove(found);
-                        options.ColorTags = tags.ToArray();
-                    }
-
-                    // Save to options
-                    await options.SaveAsync();
-                }
-            }
-        }
-    }
+					// Save to options
+					await options.SaveAsync();
+				}
+			}
+		}
+	}
 }
--- src/source.extension.cs
+++ src/source.extension.cs
@@ -11,7 +11,7 @@ namespace Highlighter
         public const string Name = "Highlighter";
         public const string Description = @"An arbitrary highlighter that lets you pick individual class, parameter, or other element names to highlight across your projects. It makes it easier to find and identify objects inside your project. You can also use it for comment annotations, dividers, and more.";
         public const string Language = "en-US";
-        public const string Version = "1.0.1.8";
+        public const string Version = "1.0.1.10";
         public const string Author = "Dax Pandhi";
         public const string Tags = "C#,Highlighting,Productivity,Color,Keyword,VB,CSS,HTML,Colorize,Tags,Mark";
     }
